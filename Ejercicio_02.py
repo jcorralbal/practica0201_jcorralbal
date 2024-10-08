aed4e72e4f66a5490e2bdac7a72e93cebb401eae
jcorralbal <jcorralbal@educacion.navarra.es>
2024-10-08 23:15:51 +0200

    Update Ejercicio_02.py

--- Ejercicio_02.py
+++ Ejercicio_02.py
@@ -1,3 +1,4 @@
-#Escribir un programa que almacene la cadena ¡Hola Mundo! en una variable y luego muestre por pantalla el contenido de la variable.
+#Escribir un programa que almacene la cadena ¡Hola Mundo! en una variable 
+#y luego muestre por pantalla el contenido de la variable.
 variable =("¡Hola Mundo!")
 print(variable)
